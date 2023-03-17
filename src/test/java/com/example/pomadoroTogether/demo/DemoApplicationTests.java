50e40d046ee85ee44d95c93daaf0d10b1e534812
nipplelion <piolotorrico@yahoo.com>
2023-03-16 22:07:45 -0700

    Add unit test for HelloWorldFile function

--- src/test/java/com/example/pomadoroTogether/demo/DemoApplicationTests.java
+++ src/test/java/com/example/pomadoroTogether/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.pomadoroTogether.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,13 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void helloWorldFileTest() {
+		DemoApplication controller = new DemoApplication();
+		String text = controller.helloWorldFile("test");
+		Assertions.assertEquals("Hello, World!", text);
 	}
 
 }
